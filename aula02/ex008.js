be066aca6c21769726cdf337aa59d59466aa0388
Luiz Felipe <luizfelipe1997@hotmail.com>
2026-09-03 17:15:55 -0300

    utilizando if e else nos estudos de javascript

---
+++ aula02/ex008.js
@@ -0,0 +1,7 @@
+var vel = 15
+console.log(`A sua velocidade e ${vel} KM por hora`)
+if (vel > 60) {
+    console.log(`Voce esta acima da velocidade permitida! MULTADO`)
+}
+
+console.log(`Dirija sempre com segurança!`)
