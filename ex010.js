42a60f6deed9c4389e0fa8cbda506dfd1cec0b55
Luiz Felipe <luizfelipe1997@hotmail.com>
2026-09-03 18:26:09 -0300

    aulas de condicoes

---
+++ ex010.js
@@ -0,0 +1,9 @@
+var idade = 66
+if (idade < 16) {
+    console.log('Voce nao vota.')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voce pode votar mas nao e obrigatorio')
+    } else {
+            console.log('O voto e obrigatorio!')
+        }
+    
